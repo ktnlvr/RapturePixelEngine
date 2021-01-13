425c55d98f18ad1e40a41db0de3db9eae4acb89d
youngman114 <artur.roos@mail.ru>
2021-01-14 01:17:46 +0300

    Indentation, namespace, spaces for tabs and such

--- RapturePixelEngine.hpp
+++ RapturePixelEngine.hpp
@@ -18,187 +18,184 @@
 // --- CLASSES AND STRUCTS ---
 // ---------------------------
 #pragma region CLASSES AND STRUCTS
-
-/// POD (Plain old data) event object
-struct Event {
-    /// Type of the occured event
-    enum class EventType : uint8_t {
-        NONE = 0,
-        KEY = 1,
-    } type;
-    
-    /// Specific for EventType::Key
-    enum class KeyEventType { 
-        PRESS = 0, 
-        RELEASE = 1,
-    };
-
-    union {
-        struct 
-        {
-            KeyEventType type;
-        } KeyEvent;
-    };
-
-    /// Create new empty event object
-    Event() : type(EventType::NONE) {}
-    /// Create a new event of a specific type
-    Event(EventType type) : type(type) {}
-
-    #ifdef __linux__
-    /// Forge a new event based on XEVENT
-    Event(const XEvent* xevent);
-    #endif
-};
-
-
-class Platform {
-protected:
-    Platform() = default;
-
-public:
-    /// Get the only Platform instance
-    static inline Platform* instance() {
-        static Platform platform;
-        return &platform;
-    }
-
-    // Singleton singleton singleton singleton
-
-    Platform(const Platform&) = delete;
-    Platform(Platform&&) = delete;
-    Platform& operator=(const Platform&) = delete;
-    Platform& operator=(Platform&&) = delete;
-
-    /// Create the window and prepare the world!
-    void CreateWindow(
-        int x = 16, 
-        int y = 16, 
-        unsigned int width = 256, 
-        unsigned int height = 256,
-        const char* title = "RapturePixelEngine Window");
-    /// Make graphics and initialize GL context
-    void CreateGraphics();
-    /// Show the window 
-    void ShowWindow();
-    /// Check if there is an event
-    bool CheckEvent(Event& out);
-
-private:
-#ifdef __linux__
-    Display* d;
-    Window w;
-#endif
-};
-
-class RapturePixelEngine {
-protected:
-    RapturePixelEngine() {
-        platform = Platform::instance();
-    }
-
-    ~RapturePixelEngine() = default;
-
-public:
-    Platform* platform = nullptr;
-
-    int x, y;
-    unsigned int width, height;
-    const char* title;
-
-    struct {
-        std::function<void(const Event&)> OnEventCallback = [](const Event&){}; 
-    } callbacks;
-
-    // Get the only RapturePixelEngine instance
-    static inline RapturePixelEngine* instance() {
-        static RapturePixelEngine engine;
-        return &engine;
-    }
-
-    // No copying or assigning, it's a singleton all in all
-
-    RapturePixelEngine(const RapturePixelEngine&) = delete;
-    RapturePixelEngine(RapturePixelEngine&&) = delete;
-    RapturePixelEngine& operator=(const RapturePixelEngine&) = delete;
-    RapturePixelEngine& operator=(RapturePixelEngine&&) = delete;
-
-    void Construct(
-        int x = 16, 
-        int y = 16, 
-        unsigned int width = 256, 
-        unsigned int height = 256,
-        const char* title = "RapturePixelEngine Window") {
+namespace rpe {
+    /// POD (Plain old data) event object
+    struct Event {
+        /// Type of the occured event
+        enum class EventType : uint8_t {
+            NONE = 0,
+            KEY = 1,
+        } type;
         
-        this->x = x, this->y = y, this->width = width, this->height = height,
-        this->title = title;
+        /// Specific for EventType::Key
+        enum class KeyEventType { PRESS = 0, RELEASE = 1, };
 
-        theThread = std::thread([]() { TheThread(); });
-    }
+        union {
+            struct 
+            {
+                KeyEventType type;
+            } KeyEvent;
+        };
 
-    void Start(bool join) {
-        isRunning = true;
-        if (join) {
-            theThread.join();
+        /// Create new empty event object
+        Event() : type(EventType::NONE) {}
+        /// Create a new event of a specific type
+        Event(EventType type) : type(type) {}
+
+        #ifdef __linux__
+        /// Forge a new event based on XEVENT
+        Event(const XEvent* xevent);
+        #endif
+    };
+
+
+    class Platform {
+    protected:
+        Platform() = default;
+
+    public:
+        /// Get the only Platform instance
+        static inline Platform* instance() {
+            static Platform platform;
+            return &platform;
         }
-    }
 
-    /// Mutex lock for thread safety
-    std::mutex mtx;
-    /// Lock to prevent changes
-    std::condition_variable lock;
-    /// Defines if the thread is active or not
-    std::atomic_bool isRunning{false};
-    /// Main Engine thread
-    std::thread theThread;
+        // Singleton singleton singleton singleton
 
-    static void TheThread() {
-        // Instance reference
-        auto instance = RapturePixelEngine::instance();
-        auto platform = instance->platform;
+        Platform(const Platform&) = delete;
+        Platform(Platform&&) = delete;
+        Platform& operator=(const Platform&) = delete;
+        Platform& operator=(Platform&&) = delete;
 
-        // Creation has to be called here, so the thread recieves control
-        platform->CreateWindow(
-            instance->x, 
-            instance->y, 
-            instance->width, 
-            instance->height, 
-            instance->title);
-        platform->CreateGraphics();
+        /// Create the window and prepare the world!
+        void CreateWindow(
+            int x = 16, 
+            int y = 16, 
+            unsigned int width = 256, 
+            unsigned int height = 256,
+            const char* title = "RapturePixelEngine Window");
+        /// Make graphics and initialize GL context
+        void CreateGraphics();
+        /// Show the window 
+        void ShowWindow();
+        /// Check if there is an event
+        bool CheckEvent(Event& out);
 
-        platform->ShowWindow();
+    private:
+    #ifdef __linux__
+        Display* d;
+        Window w;
+    #endif
+    };
 
-        // Multithreading lock
-        std::unique_lock<std::mutex> lock(instance->mtx);
+    class RapturePixelEngine {
+    protected:
+        RapturePixelEngine() {
+            platform = Platform::instance();
+        }
 
-        // Wait for isRunning to be true
-        instance->lock.wait(lock, []() {
-            return RapturePixelEngine::instance()->isRunning.load();
-        });
+        ~RapturePixelEngine() = default;
 
-        // Unlock all the locks!
-        lock.unlock();
-        instance->lock.notify_all();
+    public:
+        Platform* platform = nullptr;
 
-        for(;;) {
-            Event event;
-            if(instance->platform->CheckEvent(event)) {
-                instance->callbacks.OnEventCallback(event);
+        int x, y;
+        unsigned int width, height;
+        const char* title;
+
+        struct {
+            std::function<void(const Event&)> OnEventCallback = [](const Event&){}; 
+        } callbacks;
+
+        // Get the only RapturePixelEngine instance
+        static inline RapturePixelEngine* instance() {
+            static RapturePixelEngine engine;
+            return &engine;
+        }
+
+        // No copying or assigning, it's a singleton all in all
+
+        RapturePixelEngine(const RapturePixelEngine&) = delete;
+        RapturePixelEngine(RapturePixelEngine&&) = delete;
+        RapturePixelEngine& operator=(const RapturePixelEngine&) = delete;
+        RapturePixelEngine& operator=(RapturePixelEngine&&) = delete;
+
+        void Construct(
+            int x = 16, 
+            int y = 16, 
+            unsigned int width = 256, 
+            unsigned int height = 256,
+            const char* title = "RapturePixelEngine Window") {
+            
+            this->x = x, this->y = y, this->width = width, this->height = height,
+            this->title = title;
+
+            theThread = std::thread([]() { TheThread(); });
+        }
+
+        void Start(bool join) {
+            isRunning = true;
+            if (join) {
+                theThread.join();
             }
         }
-    }
-};
-#pragma endregion // CLASSES AND STRUCTS 
 
-// Pointer type for RapturePixelEngine
-using RapturePtr = RapturePixelEngine*;
+        /// Mutex lock for thread safety
+        std::mutex mtx;
+        /// Lock to prevent changes
+        std::condition_variable lock;
+        /// Defines if the thread is active or not
+        std::atomic_bool isRunning{false};
+        /// Main Engine thread
+        std::thread theThread;
 
+        static void TheThread() {
+            // Instance reference
+            auto instance = RapturePixelEngine::instance();
+            auto platform = instance->platform;
+
+            // Creation has to be called here, so the thread recieves control
+            platform->CreateWindow(
+                instance->x, 
+                instance->y, 
+                instance->width, 
+                instance->height, 
+                instance->title);
+            platform->CreateGraphics();
+
+            platform->ShowWindow();
+
+            // Multithreading lock
+            std::unique_lock<std::mutex> lock(instance->mtx);
+
+            // Wait for isRunning to be true
+            instance->lock.wait(lock, []() {
+                return RapturePixelEngine::instance()->isRunning.load();
+            });
+
+            // Unlock all the locks!
+            lock.unlock();
+            instance->lock.notify_all();
+
+            for(;;) {
+                Event event;
+                if(instance->platform->CheckEvent(event)) {
+                    instance->callbacks.OnEventCallback(event);
+                }
+            }
+        }
+    };
+    #pragma endregion // CLASSES AND STRUCTS 
+
+    // Pointer type for RapturePixelEngine
+    using RapturePtr = RapturePixelEngine*;
+}
 // ------------------------------
 // --- METHOD IMPLEMENTATIONS ---
 // ------------------------------ 
 #pragma region METHOD IMPLEMENTATIONS
 #ifdef __linux__
-void Platform::CreateWindow(
+void rpe::Platform::CreateWindow(
     int x, 
     int y, 
     unsigned int width, 
@@ -229,16 +226,16 @@ void Platform::CreateWindow(
     XStoreName(d, w, title);
 }
 
-void Platform::CreateGraphics() {
+void rpe::Platform::CreateGraphics() {
     // TODO
 }
 
-void Platform::ShowWindow() {
+void rpe::Platform::ShowWindow() {
     XMapWindow(d, w);
     XFlush(d);
 }
 
-bool Platform::CheckEvent(Event& out) {
+bool rpe::Platform::CheckEvent(Event& out) {
     XEvent tmp;
     bool ret = XCheckWindowEvent(d, w, 
         ExposureMask | KeyPressMask | KeyReleaseMask, 
--- RapturePixelEngineExample.cpp
+++ RapturePixelEngineExample.cpp
@@ -3,9 +3,14 @@
 #include "RapturePixelEngine.hpp"
 
 
-int main() {
+int main(int argc, char *argv[]) {
+    // if you are not feeling like writing this everywhere
+    using namespace rpe;    
+
+    // Retrieve the engine instance
     RapturePtr engine = RapturePixelEngine::instance();
 
+    // Register a callback for general event
     engine->callbacks.OnEventCallback = [](const Event& e) {
         switch (e.type)
         {
@@ -19,7 +24,11 @@ int main() {
         }
     };
 
+    // Create the instance and prepare
     engine->Construct();
+    // Run the thing and join it to the main thread
     engine->Start(true);
+
+    // whooosh and halt
     return 0;
 }
